1a9c6e9d644faf41a8b2218321a169aaf240ae50
kagehina919 <ns19041997@gmail.com>
2019-02-23 21:32:08 +0530

    Database clustering started

---
+++ realData.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('ggplot')
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn import preprocessing, cross_validation
+import pandas as pd
